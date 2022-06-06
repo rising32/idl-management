e75c42437b296a2f7346eeeb665576526a9e0b24
HuaZhe Jin <jscrazy@yandex.com>
2022-06-06 20:17:38 +0800

    unused

--- src/components/priority/PriorityTab.tsx
+++ src/components/priority/PriorityTab.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PriorityContext, TABLIST } from '../../containers/priority/MainPriorityContainer';
-import { PriorityState } from '../../modules/weekPriority';
 
 function PriorityTab() {
   const navigate = useNavigate();
