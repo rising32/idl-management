374f16545975d7ceb9cf89d1e3eb8bb8b98dd1b0
HuaZhe Jin <jscrazy@yandex.com>
2022-06-06 20:46:28 +0800

    add image

--- src/components/common/CompltedIcon.tsx
+++ src/components/common/CompltedIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { RectangleFillOnRectangleFillSvg, XMarkSvg } from '../../assets/svg';
+import { documentThumbnail } from '../../assets/images';
+import { XMarkSvg } from '../../assets/svg';
 
 interface Props {
   isSelected?: boolean;
@@ -7,11 +8,11 @@ interface Props {
 }
 const CompltedIcon = ({ isCompleted, isSelected }: Props) => {
   return (
-    <div className='w-6 h-6 flex items-center justify-center relative'>
-      <RectangleFillOnRectangleFillSvg className='w-6 h-6' />
+    <div className='w-6 h-6 flex items-center justify-center relative mr-2'>
+      <img src={documentThumbnail} className='w-5 h-5' />
       {!isCompleted ? (
         <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
-          <XMarkSvg className={`w-3 h-3 ${isSelected ? 'text-white' : 'text-rouge'}`} />
+          <XMarkSvg className='w-3 h-3 text-rouge' />
         </div>
       ) : (
         <div />
